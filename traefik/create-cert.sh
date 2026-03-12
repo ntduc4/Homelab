201e78e170dfce4536f9143798fe90acb3dc4f81
Nguyen Thien Duc <ntduc4@gmail.com>
2026-03-13 00:28:00 +0800

    Split traefik, jellyfin and arr, add dozzle

--- traefik/create-cert.sh
+++ traefik/create-cert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-source "$SCRIPT_DIR/../../.env"
+source "$SCRIPT_DIR/.env"
 mkdir -p "$SCRIPT_DIR/certs"
 
 openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
