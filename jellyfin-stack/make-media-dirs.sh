053549ad39535ddcb0f72e68243f4c227c905424
Nguyen Thien Duc <ntduc4@gmail.com>
2026-04-30 20:07:12 +0800

    Update make media dirs

--- jellyfin-stack/make-media-dirs.sh
+++ jellyfin-stack/make-media-dirs.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$SCRIPT_DIR/.env"
-mkdir -p "${MEDIA_PATH}/"/{tv,movies,downloads/{complete,incomplete}}
+mkdir -p "${MEDIA_PATH}/"/{tv,anime,movies,downloads/{complete,incomplete}}
